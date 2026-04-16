d318233eda959b6add62d1bee2bf8c0832146a8f
Qookie <nathan.duchosal@epfl.ch>
2026-04-16 17:27:13 +0200

    feat: prog-A

--- progA.c
+++ progA.c
@@ -10,6 +10,9 @@ SCIPER		: Your SCIPER numbers
 #include <stdlib.h>
 #include "function.h"
 #include "utility.h"
+#include <mpi.h>
+
+#define TAG 0
 
 int main(int argc, char *argv[]) {
     int nrounds, size;
@@ -24,23 +27,68 @@ int main(int argc, char *argv[]) {
     }
 
     /* Get the number of processes */
-    int nprocs = ??;
+    int rank, nprocs;
+    MPI_Status status;
 
-    /* Initialise model */
-    rand_gen generator = init_rand(0);
-    int *model = (int*) malloc(size * sizeof(int));
-    for(int i = 0; i < size; i++) {
-        model[i] = next_rand(generator) * MAX_VAL;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int chunk_size = size / (nprocs - 1);
+    int last_chunk_size = size - chunk_size * (nprocs-2);
+
+    if(rank == 0){
+        /* Initialise model */
+        rand_gen generator = init_rand(0);
+        int *model = (int*) malloc(size * sizeof(int));
+        for(int i = 0; i < size; i++) {
+            model[i] = next_rand(generator) * MAX_VAL;
+        }
+        free_rand(generator);
+
+        set_clock();
+        for(int round = 0; round < nrounds; round++) {
+            for (int proc = 1; proc < nprocs; proc++) {
+                int chunk = proc != (nprocs-1) ? chunk_size: last_chunk_size;
+                int offset = (proc-1)*chunk_size;
+                for (int c = 0; c < chunk; c++) {
+                    MPI_Send(&model[offset + c], 1, MPI_INT, proc, TAG, MPI_COMM_WORLD);
+                }
+            }
+
+            for (int proc = 1; proc < nprocs; proc++) {
+                for (int s = 0; s < size; s++) {
+                    int recv;
+                    MPI_Recv(&recv, 1, MPI_INT, proc, TAG, MPI_COMM_WORLD, &status);
+                    model[s] += recv;
+                }
+            }
+        }
+        /* Output stats */
+        double totaltime = elapsed_time();
+        printf("- Using %d procs for %d iterations on %d size: %.3gs.\n", nprocs, nrounds, size, totaltime);
+        write_csv(&model, 1, size, "model.csv");
+        
+    } else {
+        int chunk = rank != (nprocs-1) ? chunk_size: last_chunk_size;
+
+        int *localModel = malloc(chunk * sizeof(int));
+        int *localResult = NULL; 
+        for(int round = 0; round < nrounds; round++) {
+            for (int c = 0; c < chunk; c++) {
+                MPI_Recv(&localModel[c], 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &status);
+            }
+
+            localResult = calloc(size, sizeof(int));
+
+            compute(localModel, localResult, chunk, size);
+            for (int s = 0; s < size; s++) {
+                MPI_Send(&localResult[s], 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+            }
+            free(localResult);
+        }
+        free(localModel);
     }
-    free_rand(generator);
-
-    set_clock();
-    for(int round = 0; round < nrounds; round++) {
-        /* Write code to train for N rounds */
-    }
-
-    /* Output stats */
-    double totaltime = elapsed_time();
-    printf("- Using %d procs for %d iterations on %d size: %.3gs.\n", nprocs, nrounds, size, totaltime);
-    write_csv(&model, 1, size, "model.csv");
+    MPI_Finalize();
+    return 0;
 }
